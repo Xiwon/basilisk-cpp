1b3f240aca1be35e373da0ad67cbfe18cf1a2c71
Xiwon <3550216663@qq.com>
2025-08-25 22:04:36 +0800

    feat: add autoSource header files to include/Basilisk

--- run_all_test.py
+++ run_all_test.py
@@ -1,4 +1,11 @@
-from os import system
+import os
 
-system('cd dist3 && ctest -C Release')
-# system('cd src && pytest -n auto')
+os.system("")
+statusColor = '\033[92m'
+warningColor = '\033[93m'
+endColor = '\033[0m'
+
+print(f"{warningColor}========== run all tests =========={endColor}")
+cmd_run_tests = "cd dist3 && ctest -C Release"
+print(f"{statusColor}[command] {cmd_run_tests}{endColor}")
+os.system(cmd_run_tests)
